c787388be16f4ad3cf1e0f12f46ef6f19def8b1e
Muhammad Rehan Khalid <m.rehankhalid@gcuf.edu.pk>
2024-02-03 00:30:28 +0500

    Exploding_Pie_Donut_chart

--- Programms/Pie-Donut Chart.R
+++ Programms/Pie-Donut Chart.R
@@ -9,3 +9,12 @@ tail(data)
 # Building a table with the data for the plot
 PD = data %>% group_by(Class, Survived) %>% summarise(n = sum(Freq))
 print(PD)
+#Basic Pie-Donut chart
+# Pie-Donut chart
+PieDonut(PD, aes(Class, Survived, count=n), title = "Titanic: Survival by Class")
+# show lables over it
+PieDonut(PD, aes(Class, Survived, count=n), title = "Titanic: Survival by Class",
+         ratioByGroup = FALSE)
+# by class
+PieDonut(PD, aes(Survived, Class, count=n), title = "Titanic: Survival by Class")
+# Exploding the pie
