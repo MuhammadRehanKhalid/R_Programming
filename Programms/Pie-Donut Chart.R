6113282760819b7c8a02db023ba678712a28e1cd
Muhammad Rehan Khalid <m.rehankhalid@gcuf.edu.pk>
2024-02-03 00:27:13 +0500

    Changed_pie

--- Programms/Pie-Donut Chart.R
+++ Programms/Pie-Donut Chart.R
@@ -2,4 +2,10 @@
 library(ggplot2)
 library(webr)
 library(dplyr)
-
+# loading the dataset
+data <- as.data.frame(Titanic)
+head(data)
+tail(data)
+# Building a table with the data for the plot
+PD = data %>% group_by(Class, Survived) %>% summarise(n = sum(Freq))
+print(PD)
