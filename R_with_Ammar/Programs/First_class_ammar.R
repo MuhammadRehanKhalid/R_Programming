e1ab7c09cf9dbf5a05116bf4af913d3466019daf
Muhammad Rehan Khalid <m.rehankhalid@gcuf.edu.pk>
2024-10-16 23:38:36 +0500

    Array

--- R_with_Ammar/Programs/First_class_ammar.R
+++ R_with_Ammar/Programs/First_class_ammar.R
@@ -23,8 +23,26 @@ numeric_vec <- c(1,2,3,4,5)
 numeric_vec
 character_vec <- c("a","b","c","d","e")
 character_vec
-logical_vec <- c(TRUE, FALSE, TRUE, FALSE)
+logical_vec <- c(TRUE, FALSE, TRUE, FALSE, TRUE)
 logical_vec
-logical_vec2 <- c(T, F, T, F)
+logical_vec2 <- c(T, F, T, F, T)
 logical_vec2
 # data frames are two dimensional data structures
+# they are used to store data in rows and columns
+df  <- data.frame(numeric_vec, character_vec, logical_vec,
+                  number = c(1,2,32,4,5))
+df
+# now create a matrix
+mat <- matrix(1:9, nrow = 3, ncol = 3)
+mat
+# make another similar matarix
+mat2 <- matrix(10:18, nrow = 3, ncol = 3)
+mat2
+# dot and cross product
+mat * mat2
+# build and array
+arr <- array(1:24, dim = c(2,3,4))
+arr
+
+
+
