c569439fa4926c166ef05de95481c955596835f2
Muhammad Rehan Khalid <m.rehankhalid@gcuf.edu.pk>
2024-10-20 01:10:34 +0500

    Update Basic_Packages.R

--- R_with_Ammar/Basic_Packages.R
+++ R_with_Ammar/Basic_Packages.R
@@ -13,6 +13,8 @@ install.packages("haven")
 # install.packages("tidyr")
 ## ---- Packages_for_Data_manipulation_data_wrangling/Handling----
 install.packages("dplyr")
+install.packages("tidyr")
+install.packages("lubridate")
 # These are te he help places to learn
 ?base::Logic
 ?base::Arithmetic
@@ -31,6 +33,20 @@ install.packages("dplyr")
 
 #----Packages_for_Data_Visualization----
 install.packages("ggplot2")# check ggplot2 book and also check Andrew abbe la guide for plotting
+install.packages("plotly")
+install.packages("RcolorBrewer") # color palettes
+install.packages("leaflet") # geography
+install.packages("dygraphs") # times series
+install.packages("gganimate")
+install.packages("ggpubr")
+install.packages("heatmaply")
+install.packages("d3heatmap")
+install.packages("rgl")
+
+install.packages("viridis")
+install.packages("cowplot")
+install.packages("ggstatsplot")
+install.packages("patchwork")# combine plots
 library(ggplot2)
 df <- data.frame(mpg)
 print(df)
@@ -41,3 +57,21 @@ ggplot(df, aes(x=manufacturer, y=cty)) + geom_bin2d()
 ggplot(df, aes(x=manufacturer, y=cty)) + geom_jitter()
 ggplot(df, aes(x=manufacturer, y=cty)) + geom_violin()
 
+library(plotly)
+fig <- plot_ly(df, x = ~manufacturer, y = ~cty, type = 'scatter', mode = 'markers')
+fig
+fig2 <- plot_ly(midwest, x = ~percollege, color = ~state, type = 'box')
+fig2
+
+
+
+# ----Statistics_Machine_Learning----
+install.packages("tidymodels") # tidyverse for modeling
+install.packages("caret") # classification and regression training
+install.packages("car") # companion to applied regression
+install.packages("lme4") # linear mixed effects models
+installl.packages("multcomp") # multiple comparison
+install.packages("nlme") # linear and nonlinear mixed effects models
+install.packages("vcd") # visualizing categorical data
+install.packages("survival") # survival analysis
+install.packages("agricolae") # statistical procedures for agricultural research
