df4de56bf873473d193c40fccf198214a1116fe7
Muhammad Rehan Khalid <m.rehankhalid@gcuf.edu.pk>
2024-10-19 06:28:16 +0500

    Done

--- R_with_Ammar/Basic_Packages.R
+++ R_with_Ammar/Basic_Packages.R
@@ -1,4 +1,30 @@
 # ---- Packages and Functions----
-## ---- Data_Loading_and_Packages ----
+## ---- Data_Loading_and_Writing----
 install.packages("readr")
 install.packages("readxl")
+install.packages("tibble")
+install.packages("forcats")
+install.packages("stringr")
+install.packages("purrr")
+install.packages("ggplot2")
+install.packages("openxlsx")
+install.packages("writexl")
+install.packages("haven")
+# install.packages("tidyr")
+## ---- Packages_for_Data_manipulation_data_wrangling/Handling----
+install.packages("dplyr")
+# These are te he help places to learn
+?base::Logic
+?base::Arithmetic
+?base::Comparison
+?base::Control
+?base::Data
+?base::Math
+?base::Numeric
+?base::Complex
+
+
+
+
+
+
