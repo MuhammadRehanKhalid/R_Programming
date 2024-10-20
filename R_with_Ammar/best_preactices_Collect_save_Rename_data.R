f51367e574c3bb41479e54216f29bf90be3f4bea
Muhammad Rehan Khalid <m.rehankhalid@gcuf.edu.pk>
2024-10-20 15:35:42 +0500

    Update best_preactices_Collect_save_Rename_data.R

--- R_with_Ammar/best_preactices_Collect_save_Rename_data.R
+++ R_with_Ammar/best_preactices_Collect_save_Rename_data.R
@@ -1,5 +1,11 @@
 # 1. Start making a new file with naming convention 
   # > don't use CAPITAL letter
   # > use underscore instead of space
-  # > don't start file other than cel A1
-  
+  # > don't start file other than cell A1
+  # > you should know your header location
+  # > don't use special characters
+  # > don't use numbers in the start
+  # > don't use spaces in columns names or capital letters
+# 2. Use hide any column
+# 3. Use similar text style for everything
+  # like Pakistan, not PaKISTAN, or pakisTan etc
