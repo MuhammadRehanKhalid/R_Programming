8c15bb896c98c69b13f6163dc2083cb72a85cc1d
Muhammad Rehan Khalid <m.rehankhalid@gcuf.edu.pk>
2024-10-20 14:46:43 +0500

    New_File_Code

--- R_with_Ammar/Data_Import_Read_Export.R
+++ R_with_Ammar/Data_Import_Read_Export.R
@@ -1,6 +1,7 @@
 # readr, readxl, openxl
-install.packages("openxlsx")
+install.packages("openxlsx", dependencies =  TRUE)
 install.packages("openxl")
+library(writexl)
 library(readxl)
 library(readr)
 library(openxlsx)
@@ -8,3 +9,7 @@ library(openxl) # not found
 
 df <- read_csv("./Data/titanic.csv")
 df
+df_2 <- read_xlsx("./Data/CO2_data.xlsx", sheet = 'iris') # name is preffered
+df_2
+write_xlsx(df, "./Data/comb_CO2_Iris.xlsx")
+write_tsv(df, "./Data/comb_CO2_Iris.tsv")
