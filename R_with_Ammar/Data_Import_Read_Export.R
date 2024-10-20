e91b95cb538ab7a7aed201a1f59ed78aca9b86e6
Muhammad Rehan Khalid <m.rehankhalid@gcuf.edu.pk>
2024-10-20 13:55:36 +0500

    New_data_sets

--- R_with_Ammar/Data_Import_Read_Export.R
+++ R_with_Ammar/Data_Import_Read_Export.R
@@ -1,1 +1,10 @@
 # readr, readxl, openxl
+install.packages("openxlsx")
+install.packages("openxl")
+library(readxl)
+library(readr)
+library(openxlsx)
+library(openxl) # not found
+
+df <- read_csv("./Data/titanic.csv")
+df
